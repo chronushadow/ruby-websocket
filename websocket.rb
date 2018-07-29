c73a59fdf1e03546919b8911955cc21bcebe47c6
chronushadow <chronushadow@gmail.com>
2018-07-29 14:41:24 +0900

    Add calculating of time from client to server.

--- websocket.rb
+++ websocket.rb
@@ -1,16 +1,23 @@
 require 'faye/websocket'
+require 'time'
 
 App = lambda { |env|
   if Faye::WebSocket.websocket?(env)
     ws = Faye::WebSocket.new(env)
 
     ws.on :open do |event|
-      puts "WebSocket connection open"
+      puts "Connection opened"
     end
 
     ws.on :message do |event|
+      recieved_time = Time.now
+
       puts "Recieved message: #{event.data}"
-      ws.send("Pong: #{event.data}")
+      client_send_time = Time.iso8601(event.data)
+
+      puts "Time from client to server: #{recieved_time - client_send_time} [s]"
+
+      ws.send(Time.now.strftime("%Y-%m-%dT%H:%M:%S.%6N"))
     end
 
     ws.on :close do |event|
